0ce23435b121385e5b336a270e851df4e325cc41
chaedongyeong <ydc4739@naver.com>
2024-12-05 04:01:59 +0900

    README.md

---
+++ src/test/java/com/sbs/java/text_board/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.java.text_board.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
